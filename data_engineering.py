eaacfeb8468c8a98629781372c393caa1fc8480b
Dario Sandrucci <48436@novasbe.pt>
2022-10-19 12:31:33 +0100

    test train implemented

--- data_engineering.py
+++ data_engineering.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime as dt
 import pandas_datareader as pdr
 from yahoo_fin import stock_info as si
+from sklearn.model_selection import TimeSeriesSplit
 
 # Getting the data
 
@@ -41,3 +42,32 @@ def getData(startDate, endDate, tickers, downloaded = True):
 
 def getCorrMatrix(returns):
     return np.corrcoef(returns, rowvar=False)
+
+def testTrainSplit(df, validation_set = True, w_tt = [0.2, 0.8], w_tvt = [0.4,0.4,0.2]):
+    if validation_set == True:
+        if sum(w_tvt) == 1: pass
+        else: raise NameError("sum of w must be 1")
+        
+        len_df = len(df.index)
+        len_train = round(len_df*w_tvt[0])
+        len_val = round(len_df*w_tvt[1])
+        len_test = len_df - len_train - len_val
+
+        train_set = df.iloc[0:len_train-1]
+        val_set = df.iloc[len_train-1:len_train+len_val-1]
+        test_set = df.iloc[len_train+len_val-1:len_train+len_val+len_test-1]
+
+        return train_set, val_set, test_set
+
+    else:
+        if sum(w_tt) == 1: pass
+        else: raise NameError("sum of w must be 1")
+        
+        len_df = len(df.index)
+        len_train = round(len_df*w_tt[0])
+        len_test = len_df - len_train 
+
+        train_set = df.iloc[0:len_train-1]
+        test_set = df.iloc[len_train-1:len_train+len_test-1]
+
+        return train_set, test_set
--- main.py
+++ main.py
@@ -6,26 +6,10 @@ tickers = getTickers()
 end = dt.now()
 start = dt.fromisoformat('2012-10-12')
 
-data = getData(start, end, tickers, downloaded = True)
-corr = getCorrMatrix(data)
+df = getData(start, end, tickers, downloaded = True)
 
-#method = [1,2]
-#for m in method:
-#    eVal0, eVec0, eVal1, eVec1, corr1, var0 = denoiseMatrix(corr, method = m)
-#    plotEvalDiff(eVal0, eVal1, method = m)
+train_set, val_set, test_set = testTrainSplit(df)
 
-eVal0, eVec0, eVal1, eVec1, corr1, var0 = denoiseMatrix(corr, method = 1)
-print("The data was denoised successfully!")
-
-corr2 = detoneMatrix(corr1, eVal1, eVec1, mc = 1)
-print("The data was detoned successfully!")
-corr2 = pd.DataFrame(corr2)
-
-#corr3, clstrs, silh_coef_optimal = clusterKMeansBase(corr2)
-#print("The data was clustered successfully!")
-
-#corr3 = pd.DataFrame(corr3)
-
-nco = optPort_nco(corr2, maxNumClusters = 3)
-
-print(nco)
+print(train_set)
+print(val_set)
+print(test_set)
